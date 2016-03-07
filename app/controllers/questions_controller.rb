b47c265933f5b913b8d85d3ef5c7809beb8961c5
muqsitnawaz <muqsitnawaz@gmail.com>
2016-03-07 16:15:55 +0500

    deleting question done

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -7,7 +7,7 @@ class QuestionsController < ApplicationController
     
     def create
         @course = Course.find_by_id(params[:course_id])
-        @cthread = @course.cthreads.find_by_id(params[:cthread_id])
+        @cthread = Cthread.find_by_id(params[:cthread_id]);
 
         q_params = question_params
         q_params[:cthread_id] = params[:cthread_id]
@@ -21,20 +21,36 @@ class QuestionsController < ApplicationController
     def edit
         # accessing variables
         @course = Course.find_by_id(params[:course_id])
-        @cthread = @course.cthreads.find_by_id(params[:cthread_id]);
-        @question = @cthread.questions.find_by_id(params[:id])
+        @cthread = Cthread.find_by_id(params[:cthread_id]);
+        @question = Question.find_by_id(params[:id])
     end
 
     def update
         @course = Course.find_by_id(params[:course_id])
-        @cthread = @course.cthreads.find_by_id(params[:cthread_id])
-        @question = @cthread.questions.find_by_id(params[:id])
+        @cthread = Cthread.find_by_id(params[:cthread_id]);
+        @question = Question.find_by_id(params[:id])
 
         q_params = question_params
         q_params[:cthread_id] = params[:cthread_id]
 
         @question.update_attributes! q_params
         flash[:notice] = "Question for #{@cthread.title}, #{@course.title} was successfully updated."
+        
+        redirect_to course_cthread_path(@course, @cthread)
+    end
+
+    def destroy 
+        # accessing variables
+        @course = Course.find_by_id(params[:course_id])
+        @cthread = Cthread.find_by_id(params[:cthread_id])
+        @question = Question.find_by_id(params[:id])
+       
+        if @question.nil?
+            flash[:notice] = "Question doesn't exist"
+        else
+            @question.destroy
+            flash[:notice] = "#{@question.heading} was deleted successfully."
+        end
         
         redirect_to course_cthread_path(@course, @cthread)
     end
